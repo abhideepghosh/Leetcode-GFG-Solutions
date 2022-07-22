437a728bb76cc737482bf41bd8e63919cf6a6183
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-07-22 22:52:03 +0530

    Create SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for(int i = 1; i< nums.length;i++) {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
