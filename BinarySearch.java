8cf3e4737b3a9987b5a3e2c906a13700e424b37a
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-07-24 11:13:32 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,7 +1,7 @@
 class Solution {
     public int search(int[] nums, int target) {
         int index = Arrays.binarySearch(nums, target);
-        if(index <0){
+        if(index <0){       ////This condition becomes necessary because the library function at times returns values >-1
             index = -1;
         }
         return index;
