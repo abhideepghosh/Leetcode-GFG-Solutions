a8c3428b08f2aaba4086d68cf2eba75b568840dc
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-03 23:46:49 +0530

    Update CountPrimes.java

--- CountPrimes.java
+++ CountPrimes.java
@@ -1,5 +1,32 @@
 class Solution {
     public int countPrimes(int n) {
+        
+       boolean[] primes = new boolean[n];
+        
+        for(int i=2; i*i<n ; i++){
+            if(!primes[i]){
+                
+                for(int j = i*i; j<n; j+=i){
+                    primes[j] = true;
+                }
+            }
+        }
+        
+        int count =0;
+        
+        for(int i = 2; i <n; i++){
+            if(!primes[i]){
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
+
+/*
+class Solution {
+    public int countPrimes(int n) {
        boolean[] primes = new boolean[n];
         for(int i=0; i<n; i++){
             primes[i] = true;
@@ -21,3 +48,4 @@ class Solution {
         return primeCount;
     }
 }
+*/
