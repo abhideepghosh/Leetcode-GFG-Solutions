92da825f79a8e13999b84a53e06f814be1a25cd7
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-07 12:54:55 +0530

    Update ValidAnagram.java

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] charCount = new int[128];
+        int[] charCount = new int[26];
         for(char c : s.toCharArray()){
-            charCount[c]++;
+            charCount[c-'a']++;
         }
-        int[] charCountT = new int[128];
+        int[] charCountT = new int[26];
         for(char c : t.toCharArray()){
-            charCountT[c]++;
+            charCountT[c-'a']++;
         }
-        for(int i=0; i<128; i++){
+        for(int i=0; i<26; i++){
             if(charCount[i] != charCountT[i]){
                 return false;
             }
@@ -16,6 +16,27 @@ class Solution {
         return true;
     }
 }
+
+
+/*
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] str = s.toCharArray();
+        char[] ttr = t.toCharArray();
+        
+        Arrays.sort(str);
+        Arrays.sort(ttr);
+        String sol1 = new String(str);
+        String sol2 = new String(ttr);
+        
+        if(sol1.equals(sol2))
+            return true;
+        
+        else return false;
+    }
+} 
+
+*/
 
 
 /*
