880dd129c8ee12efdaaf868e7c17c48daf994b1d
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-10 19:42:04 +0530

    Update LongestCommonPrefix.java

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,5 +1,8 @@
+/*
+//Brute Force
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        
         
         if(strs.length == 0){
             return "";
@@ -15,4 +18,25 @@ class Solution {
         return finder;
     }
         
+}
+*/
+//Optimised O(logn) Approach
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+                if (strs == null || strs.length == 0) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        Arrays.sort(strs);
+        char[] first = strs[0].toCharArray();
+        char[] last = strs[strs.length - 1 ].toCharArray();
+        for (int i = 0; i < first.length && i < last.length; i++) {
+            if (first[i] == last[i]) {
+                result.append(first[i]);
+            }
+            else
+                return result.toString();
+        }
+        return result.toString();
+    }
 }
