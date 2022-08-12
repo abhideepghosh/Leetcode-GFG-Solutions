9e323e9d80d26ed9fc2f7dd1ada8dd9ddd516c39
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-12 12:11:49 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,37 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode res = new ListNode(0);
-        ListNode temp = res;
+        ListNode result = new ListNode(0);
+        ListNode head = result;
         int carry = 0;
-        while(l1 != null || l2 != null){
-            int l1_val = (l1 != null) ? l1.val : 0;
-            int l2_val = (l2 != null) ? l2.val : 0;
-            int currSum = l1_val + l2_val + carry;
+        while(l1 != null || l2 !=null || carry != 0){
+            int l1_val = (l1 != null)? l1.val : 0;
+            int l2_val = (l2 != null)? l2.val : 0;
             
-            carry = currSum/10;
+            int res = l1_val + l2_val + carry;
             
-            int lastDigit = currSum%10;
+            carry = res/10;
             
-            ListNode newListNode = new ListNode(lastDigit);
-            temp.next = newListNode;
-            temp = temp.next;
+            int lastDigit = res%10;
             
-            if(l1 != null){
-                l1 = l1.next;
-            }
-            
-            if(l2 != null){
-                l2 = l2.next;
-            }
+            ListNode digit = new ListNode(lastDigit);
+            head.next = digit;
+            head = head.next;
+            if(l1 != null)
+            l1 = l1.next;
+            if(l2 != null)
+            l2 = l2.next;
             
         }
-        
-        if(carry > 0){
-            ListNode newListNode = new ListNode(carry);
-            temp.next = newListNode;
-        }
-        
-        return res.next;
-        
+        return result.next;
     }
 }
