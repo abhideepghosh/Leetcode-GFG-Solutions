097131f5cd85510c549b47e332f3c58a0f5038aa
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-15 13:40:15 +0530

    Update BackspaceStringCompare.java

--- BackspaceStringCompare.java
+++ BackspaceStringCompare.java
@@ -1,46 +1,71 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Backspace String Compare.
+Memory Usage: 40.5 MB, less than 93.18% of Java online submissions for Backspace String Compare.
+*/
+// Optimized Solution TC: O(n) SC: O(1)
+/*
+TC: O(n) Explanation:
+    1. Because length of the String cannot be more than 200 as per the Question Constraints.
+    2. Hence, all the inner loops have a constant Time Complexity
+*/
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int sPointer = s.length()-1;
-        int tPointer = t.length()-1;
-        int sSkip = 0;
-        int tSkip = 0;
-        
-        
-        while(sPointer >= 0 || tPointer >= 0){
-            while(sPointer >= 0){
-                if(s.charAt(sPointer) == '#'){
-                    sPointer--;
-                    sSkip++;
-                }else if(sSkip>0){
-                    sPointer--;
-                    sSkip--;
+        int i = s.length()-1;
+        int j = t.length()-1;
+        int hashCountS = 0;
+        int hashCountT = 0;
+        while(i>=0 || j >=0){
+            while(i >= 0){
+                if(s.charAt(i) == '#'){
+                    hashCountS++;
+                    i--;
+                }else if(hashCountS >0){
+                    hashCountS--;
+                    i--;
                 }else{
                     break;
                 }
             }
-            while(tPointer >= 0){
-                if(t.charAt(tPointer) == '#'){
-                    tPointer--;
-                    tSkip++;
-                }else if(tSkip>0){
-                    tPointer--;
-                    tSkip--;
+            while(j >= 0){
+                if(t.charAt(j) == '#'){
+                    hashCountT++;
+                    j--;
+                }else if(hashCountT >0){
+                    hashCountT--;
+                    j--;
                 }else{
                     break;
                 }
             }
-            
-            if(sPointer >= 0 && tPointer >= 0 && (s.charAt(sPointer) != t.charAt(tPointer))){
+            if(i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j)){
                 return false;
             }
-            
-            if((sPointer >= 0) != (tPointer >= 0)){
+            if((i >= 0) != (j>=0)){
                 return false;
             }
-            
-            sPointer--;
-            tPointer--;
+            i--;
+            j--;
         }
         return true;
     }
 }
+
+/*
+//Brute Force TC: O(n) SC: O(n)
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return buildString(s).equals(buildString(t));
+    }
+    
+    public String buildString(String s){
+        LinkedList<Character> list = new LinkedList<>();
+        for(char c : s.toCharArray()){
+            if(c!='#')
+                list.add(c);
+            else if(!list.isEmpty())
+                list.removeLast();
+        }
+        return String.valueOf(list);
+    }
+}
+*/
