579cebe80bf8664747cbca9c8d0a122ff7f0b96f
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-15 14:48:54 +0530

    Create RomanToInteger.java

---
+++ RomanToInteger.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int romanToInt(String s) {
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        int res = 0;
+        
+        for(int i=0; i<s.length(); i++){
+            
+            if(i>0 && map.get(s.charAt(i)) > map.get(s.charAt(i-1))){
+                res += map.get(s.charAt(i)) - 2*map.get(s.charAt(i-1));
+            }else{
+                res += map.get(s.charAt(i));   
+            }
+        }
+        return res;
+    }
+}
