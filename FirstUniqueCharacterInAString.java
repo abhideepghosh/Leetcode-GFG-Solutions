9750358842c1bd8f920d8da464852f78708acf72
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-16 09:38:04 +0530

    Update FirstUniqueCharacterInAString.java

--- FirstUniqueCharacterInAString.java
+++ FirstUniqueCharacterInAString.java
@@ -1,35 +1,34 @@
+// Optimised Solution TC: O(n) SC: O(1)
 class Solution {
     public int firstUniqChar(String s) {
-        int[]count=new int[26];
-        for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
+        int[] charCount = new int[26]; // Constant Space
+        for(char c : s.toCharArray()){
+            charCount[c-'a']++; // Incrementing the value of index of the 26 lowercase characters
         }
-        for(int i=0;i<s.length();i++){
-            if(count[s.charAt(i)-'a']==1) return i;
+        for(int i=0; i<s.length(); i++){
+            char currentChar = s.charAt(i);
+            if(charCount[currentChar - 'a'] == 1){
+                return i;
+            }
         }
         return -1;
     }
 }
 
-// class Solution {
-//     public int firstUniqChar(String s) {
-//         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-//         for(int i=0 ; i<s.length(); i++){
-//             if(!map.containsKey(s.charAt(i))){
-//                 map.put(s.charAt(i), 1);
-//             }else{
-//                 map.put(s.charAt(i), 2);
-//             }
-//         }
-//         for(int i = 0; i < s.length(); i++){
-//             char ch = s.charAt(i);
-//             if(map.containsKey(ch) && (map.get(ch) == 1)){
-//                 return i;
-//             } 
-//         }
-        
-//         return -1;
-//     }
-        
-    
-// }
+/*
+// Brute Force TC: O(n) SC: O(n)
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        for(int i=0; i<s.length(); i++){
+            if(map.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+*/
