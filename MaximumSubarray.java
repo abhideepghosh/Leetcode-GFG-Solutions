8d1f70dc09e8635385ab85dba7c56519c2d37909
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-22 11:39:13 +0530

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,20 +1,28 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currSum = nums[0];
+        
+        // The Problem Asks For Kadane's Algorithm
+        int currentSum = nums[0];
         int maxSum = nums[0];
+        
+        // Base Condition Of Kadane's Algorithm:
+        // 1. if The Sum Till Now Is Greater Than Or Equal To Zero Then Keep Going.
+        // 2. if its not then, change the currentSum with the next value in array.
+        // 3. Keep Storing the maxValue or the peak of the sum in a variable.
+        // 4. Return the final maxValue.
         for(int i=1; i<nums.length; i++){
-            if(currSum >= 0){
-                currSum += nums[i];
+
+            if(currentSum >= 0){
+                currentSum += nums[i];
             }else{
-                currSum  = nums[i];
+                currentSum = nums[i];
             }
             
-            if(maxSum < currSum){
-                maxSum = currSum;
+            if(currentSum > maxSum){
+                maxSum = currentSum;
             }
+            
         }
-        
         return maxSum;
-        
     }
 }
