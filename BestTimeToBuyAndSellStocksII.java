a282c1c809e445c5bffe3389a087f173461e15e2
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-22 13:18:12 +0530

    Update BestTimeToBuyAndSellStocksII.java

--- BestTimeToBuyAndSellStocksII.java
+++ BestTimeToBuyAndSellStocksII.java
@@ -1,3 +1,21 @@
+// Optimised Solution TC: O(n) SC: O(1)
+/*
+Runtime: 1 ms, faster than 97.51% of Java online submissions for Best Time to Buy and Sell Stock II.
+Memory Usage: 44.5 MB, less than 39.82% of Java online submissions for Best Time to Buy and Sell Stock II.
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
+
+
+// Brute Force Solution TC: O(n) SC: O(1)
 class Solution {
     public int maxProfit(int[] a){
         
