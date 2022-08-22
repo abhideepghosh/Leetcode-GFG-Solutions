47c18a2f340a9a8f134543b276c44ac55ffadf66
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-22 13:10:38 +0530

    Update BestTimetoBuyandSellStock.java

--- BestTimetoBuyandSellStock.java
+++ BestTimetoBuyandSellStock.java
@@ -1,16 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        
         int maxProfit = 0;
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<prices.length; i++){
-            if(prices[i]<min){
-                min = prices[i];
-            }else{
-                maxProfit = Math.max(maxProfit, (prices[i]-min));
+        int minimumSellPrice = Integer.MAX_VALUE;
+        
+        for(int price : prices){
+            if(minimumSellPrice > price){
+                minimumSellPrice = price;
             }
             
+            maxProfit = Math.max(maxProfit, price - minimumSellPrice);
+            
         }
-        
         return maxProfit;
     }
 }
