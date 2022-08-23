46d307a98e855b3acd93bc264d5a0380553b6a7a
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-23 11:55:04 +0530

    Update RotateImage.java

--- RotateImage.java
+++ RotateImage.java
@@ -1,19 +1,40 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Rotate Image.
+Memory Usage: 43.1 MB, less than 30.35% of Java online submissions for Rotate Image.
+*/
+/*
+    | TC: O(n) (n = Number of cells in the grid) | SC: O(1) |
+    
+    Explanation of Time Complexity:
+    1. Transposing the matrix has a cost of O(n)(we're moving the value of each cell once).
+    2. Reversing each row also has a cost of O(n) (we're moving the value of each cell once).
+
+
+*/
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
+        
+        int numRows = matrix.length;
+        
+        // Transpose The Matrix
+        for(int i=0; i<numRows; i++){
+            for(int j=i; j<numRows; j++){
+                
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
+                
             }
         }
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n/2; j++){
+        // Swap The Elements From Both Side Moving Towards The Centre
+        for(int i=0; i<numRows; i++){
+            for(int j = 0; j<(numRows/2); j++){
+                
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-1-j];
-                matrix[i][n-1-j] = temp;
+                matrix[i][j] = matrix[i][numRows - 1 - j];
+                matrix[i][numRows - 1 - j] = temp;
+                
             }
         }
     }
