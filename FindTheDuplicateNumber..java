22fff0663f54f7718f0e8b57ebc65f1c1fca35b3
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-24 13:25:56 +0530

    Update FindTheDuplicateNumber..java

--- FindTheDuplicateNumber..java
+++ FindTheDuplicateNumber..java
@@ -12,3 +12,33 @@ class Solution {
         
     }
 }
+
+
+// Brute Force Solution TC: O(nlogn) SC: O(1)
+class Solution {
+    
+    public int findDuplicate(int[] nums) {
+        // 'low' and 'high' represent the range of values of the target        
+        int low = 1, high = nums.length - 1;
+        int duplicate = -1;
+        
+        while (low <= high) {
+            int cur = (low + high) / 2;
+
+            // Count how many numbers in 'nums' are less than or equal to 'cur'
+            int count = 0;
+            for (int num : nums) {
+                if (num <= cur)
+                    count++;
+            }
+            
+            if (count > cur) {
+                duplicate = cur;
+                high = cur - 1;
+            } else {
+                low = cur + 1;
+            }
+        }
+        return duplicate;
+    }
+}
