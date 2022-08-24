e204299c556f5adeef32855674c959b6cae52ae9
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-24 11:38:47 +0530

    Update MergeSortedArray.java

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,35 +1,30 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Sorted Array.
+Memory Usage: 42.3 MB, less than 91.21% of Java online submissions for Merge Sorted Array.
+*/
+// Optimized Solution TC: O(n) SC: O(1)
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
         
-        int[] nums = new int[m];
-        for(int t=0; t<m; t++){
-            nums[t] = nums1[t];
-        }
+        int index = nums1.length - 1;   // Index for insertion (From The End)
+        int i = m-1;                    // Index for nums1 (From The End)
+        int j = n-1;                    //Index for nums2 (From The End)
         
-        while(i<m && j<n){
-            if(nums[i] < nums2[j]){
-                nums1[k] = nums[i];
-                i++;
+        // Traversing From The End Of The Array Inserting Elements In Sorted Manner
+        while(index >= 0){
+            
+            int nums1Value = (i >= 0) ? nums1[i] : Integer.MIN_VALUE;
+            int nums2Value = (j >= 0) ? nums2[j] : Integer.MIN_VALUE;
+            
+            if(nums1Value > nums2Value){
+                nums1[index] = nums1Value;
+                i--;
             }else{
-                nums1[k] = nums2[j];
-                j++;
+                nums1[index] = nums2Value;
+                j--;
             }
             
-            k++;
-        }
-        
-        while(i<m){
-            nums1[k] = nums[i];
-            i++;
-            k++;
-        }
-        while(j<n){
-            nums1[k] = nums2[j];
-            j++;
-            k++;
+            index--;
         }
     }
 }
