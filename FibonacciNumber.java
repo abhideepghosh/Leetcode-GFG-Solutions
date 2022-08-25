08649e2257895c4da9c521ee5334f30659b90bd7
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-25 13:22:20 +0530

    Update FibonacciNumber.java

--- FibonacciNumber.java
+++ FibonacciNumber.java
@@ -1,37 +1,26 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Fibonacci Number.
+Memory Usage: 41.1 MB, less than 32.82% of Java online submissions for Fibonacci Number.
+*/
 class Solution {
-    //Iterative Way --> 100% Faster, 0ms.
     public int fib(int n) {
-        int solution=1;
-        int count =0;
-        int temp =0;
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        for(int i=2; i<=n;i++){
-            temp=solution;
-            solution=solution+count;
-            count=temp;
-        }
-        return solution;
-    }
-    
-   /*
-   // Recursive Way
-   
-   public int fib(int n) {
         
-        if(n==0){
+        if(n == 0){
             return 0;
         }
-        if(n==1){
+        if(n == 1){
             return 1;
         }
-        return fib(n-1) + fib(n-2);
+        
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+        
     }
-   
-   */
-    
 }
