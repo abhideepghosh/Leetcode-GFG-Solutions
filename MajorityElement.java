c8d44c4cec47ec960b7ec8b4a5c8d3c2b4f4d5d5
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-29 11:43:26 +0530

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,17 +1,33 @@
+// Approach: Boyer-Moore Voting Algorithm TC: O(n) SC: O(1)
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int maxElement = 0;
-        for(int i=0; i<nums.length; i++){
+        
+        // Initialising currentElement With The Value Of First Element
+        int currentElement = nums[0];
+        // Setting Its Count As 1, Count Will Keep A Check On The Frequency Of The Element
+        int count = 1;
+        
+        for(int i=1; i<nums.length; i++){
+            
+            /*
+            The Intuition Here Is That The Number Having  ArraySize/2 Frequency Will Cancel Out 
+            All The Frequencies Of Other Elements. With This Intuition We Check For Frequencies Of Elements 
+            And Use Base If Else Check Conditions.
+            */
+            
             if(count == 0){
-                maxElement = nums[i];
+                currentElement = nums[i];
             }
-            if(nums[i] == maxElement){
+            
+            if(currentElement == nums[i]){
                 count++;
             }else{
                 count--;
             }
         }
-        return maxElement;
+        
+        //Returning The Element With Highest ArraySize/2 Frequency
+        return currentElement;
     }
 }
