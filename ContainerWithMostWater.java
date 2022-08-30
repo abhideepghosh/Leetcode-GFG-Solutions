63aba6de9c48312f2d5f065a204542413a5c4499
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-30 16:50:18 +0530

    Update ContainerWithMostWater.java

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -1,17 +1,43 @@
+/*
+Runtime: 3 ms, faster than 97.52% of Java online submissions for Container With Most Water.
+Memory Usage: 81.6 MB, less than 19.92% of Java online submissions for Container With Most Water.
+*/
 class Solution {
     public int maxArea(int[] height) {
+        
+        // Defining 2 Pointers At The End Of The Array
         int leftPointer = 0;
-        int rightPointer = height.length-1;
-        int maxArea = 0;
-        while(leftPointer<rightPointer){
-            if(height[leftPointer]<height[rightPointer]){
-                maxArea = Math.max(maxArea, height[leftPointer]*(rightPointer-leftPointer));
+        int rightPointer = height.length - 1;
+        
+        // Defining the int Variable To Store The Final Result
+        int maxWaterInContainer = 0;
+        
+        /* 
+        Approach:
+           Checking From Both Sides, we find which height is smaller in the array and multiply it with the distance
+           between both the points.
+           Then we store its value based on whether the already stored max value is larger or not than the current 
+           area value.
+           We are multiplying height with distance in order to find the area of the rectangle formed.
+           Then we increment the leftPointer or decrement the rightPointer based on whose height value is smaller.
+        */
+        while(leftPointer < rightPointer){
+            
+            int waterContained = 0;
+            
+            if(height[leftPointer] < height[rightPointer]){
+                waterContained = height[leftPointer] * (rightPointer - leftPointer);
                 leftPointer++;
             }else{
-                maxArea = Math.max(maxArea, height[rightPointer]*(rightPointer-leftPointer));
+                waterContained = height[rightPointer] * (rightPointer - leftPointer);
                 rightPointer--;
             }
+            
+            maxWaterInContainer = Math.max(maxWaterInContainer, waterContained);
         }
-        return maxArea;
+        
+        // Returning The Final Result
+        return maxWaterInContainer;
+        
     }
 }
