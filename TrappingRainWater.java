182c619e40e7d44047a32121a5ab5ce78120764a
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-08-30 18:35:25 +0530

    Update TrappingRainWater.java

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -1,26 +1,49 @@
+/*
+Runtime: 1 ms, faster than 99.76% of Java online submissions for Trapping Rain Water.
+Memory Usage: 48.8 MB, less than 52.21% of Java online submissions for Trapping Rain Water.
+*/
+// Optimized Solution TC: O(n) SC: O(n)
 class Solution {
-    public static int trap(int[] height) {
-		int j= height.length-1;
-		int[] left = new int[height.length];
-		int[] right = new int[height.length];
-		int leftMax =0;
-		int rightMax =0;
-		
-		int result =0;
-		
-		for(int i=0;i<height.length;i++) {
-			leftMax = Math.max(leftMax, height[i]);
-			left[i] = leftMax;
-			rightMax = Math.max(rightMax, height[j]);
-			right[j] = rightMax;
-			j--;
-		}
-		
-		for(int i=0;i<height.length;i++) {
-			result+= Math.min(left[i],right[i])-height[i];
-		}
-		
-		return result;
-
-	}
+    public int trap(int[] height) {
+        
+        int heightLength = height.length;
+        int[] leftMaxSize = new int[heightLength];
+        int[] rightMaxSize = new int[heightLength];
+        
+        // Storing The Total Amount Of Water 
+        int amountOfWaterStored = 0;
+        
+        /*
+        Approach:
+          1. We Calculate The Max Height From Left And Right, Then Taking Their Minimum And Subtracting It
+             With The Current Height Will Give Us The Amount Of Water Stored Above The Current Element.
+          2. Then We Keep Adding The Amount Of Water To The Total Amount Of Water And Finally We Return The 
+             Answer.
+        */
+        int currentMax = 0;
+        
+        for(int i=0; i<heightLength; i++){
+            if(currentMax < height[i]){
+                currentMax = height[i];
+            }
+            leftMaxSize[i] = currentMax;
+        }
+        
+        currentMax = 0;
+        
+        for(int i=heightLength-1; i>=0; i--){
+            if(currentMax < height[i]){
+                currentMax = height[i];
+            }
+            rightMaxSize[i] = currentMax;
+        }
+        
+        for(int i=0; i<heightLength; i++){
+            amountOfWaterStored += Math.min(leftMaxSize[i], rightMaxSize[i]) - height[i];
+        }
+        
+        // Returning The Total Amount Of Water Stored.
+        return amountOfWaterStored;
+        
+    }
 }
