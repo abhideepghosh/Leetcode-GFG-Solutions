103b51b05798656ca0d62e5462dbbb289002b3b9
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-01 11:56:17 +0530

    Update FindTheDuplicateNumber..java

--- FindTheDuplicateNumber..java
+++ FindTheDuplicateNumber..java
@@ -14,6 +14,29 @@ class Solution {
 }
 
 
+// Better Optimized Solution TC: O(n) SC: O(n)
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+        // Using HashSet To Store Unique Values
+        HashSet<Integer> set = new HashSet<>();
+        
+        //Traversing Through The Array
+        for(int num : nums){
+            
+            // Whenever set.add() returnns false, Return The Number As Its A Duplicate
+            if(!set.add(num)){
+                //Returning Duplicate
+                return num;
+            }
+        }
+        
+        // Default Return In Case No Duplicate Is Found
+        return -1;
+    }
+}
+
+
 // Brute Force Solution TC: O(nlogn) SC: O(1)
 class Solution {
     
