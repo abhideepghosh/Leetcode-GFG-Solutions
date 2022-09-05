f9040d0a89030ff3d101110eacb749075015f24a
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-05 11:21:33 +0530

    Update BestTimetoBuyandSellStock.java

--- BestTimetoBuyandSellStock.java
+++ BestTimetoBuyandSellStock.java
@@ -1,17 +1,28 @@
+/*
+Runtime: 2 ms, faster than 93.61% of Java online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 84 MB, less than 34.66% of Java online submissions for Best Time to Buy and Sell Stock.
+*/
+// TC: O(n) SC: O(1)
 class Solution {
     public int maxProfit(int[] prices) {
         
+        // Declaring And Initialising Variables To Store Result And Minimum Value Of Prices 
         int maxProfit = 0;
-        int minimumSellPrice = Integer.MAX_VALUE;
+        int minimumValue = Integer.MAX_VALUE;
         
+        // Loop To Store Minimum Price In currentValue & Calculating Profit With Every Element Greater Than currentValue
         for(int price : prices){
-            if(minimumSellPrice > price){
-                minimumSellPrice = price;
+            
+            if(price < minimumValue){
+                minimumValue = price;
+            }else{
+                // Calculating The Max Profit
+                maxProfit = Math.max(maxProfit, (price - minimumValue));   
             }
             
-            maxProfit = Math.max(maxProfit, price - minimumSellPrice);
-            
         }
+        
+        //Returning The Final Result
         return maxProfit;
     }
 }
