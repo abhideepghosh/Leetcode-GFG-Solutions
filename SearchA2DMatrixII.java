fa1fb4b800e8f38c0c0020a7d7ce83d09408da19
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-05 14:27:20 +0530

    Update SearchA2DMatrixII.java

--- SearchA2DMatrixII.java
+++ SearchA2DMatrixII.java
@@ -1,8 +1,44 @@
 /*
+Runtime: 5 ms, faster than 100.00% of Java online submissions for Search a 2D Matrix II.
+Memory Usage: 48.7 MB, less than 86.10% of Java online submissions for Search a 2D Matrix II.
+*/
+// Optimized Solution TC: O(logn)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int rowLength = matrix.length;
+        int colLength = matrix[0].length;
+        
+        int start =0;
+        int end = colLength-1;
+        
+        // Applying Binary Search Directly On The 2D Array
+        while(start < rowLength && end >= 0){
+            
+            if(matrix[start][end] == target){
+                return true;
+            }
+            else if(matrix[start][end] > target){
+                end--;
+            }
+            else {
+                start++;
+            }
+            
+        }
+        // Defauly Return Statement As False In Case The Element Was Not Found In The 2D Matrix
+        return false;
+        
+    }
+}
+
+
+
+/*
 Runtime: 6 ms, faster than 91.81% of Java online submissions for Search a 2D Matrix II.
 Memory Usage: 48.6 MB, less than 86.90% of Java online submissions for Search a 2D Matrix II.
 */
-// TC: O(n) * O(logn) = O(nlogn)
+// Brute Force Solution TC: O(n) * O(logn) = O(nlogn)
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
