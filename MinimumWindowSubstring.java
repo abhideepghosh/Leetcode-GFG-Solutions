a9376e3f2f30d6a02d1cf2d253f8198a733a9e3d
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-08 12:57:11 +0530

    Update MinimumWindowSubstring.java

--- MinimumWindowSubstring.java
+++ MinimumWindowSubstring.java
@@ -1,29 +1,41 @@
+// Time Complexity O(n^2) Space Complexity O(n)
 class Solution {
     public String minWindow(String s, String t) {
         
+        // For Storing Result
         String result = "";
         
+        // DesiredStr - Used For Storing t
+        //findStr - Used For Storing The Window
         HashMap<Character, Integer> desiredStr = new HashMap<>();
         HashMap<Character, Integer> findStr = new HashMap<>();
         
+        // Filling desiredStr With Characters And Their Frequencies
         for(int i=0; i<t.length(); i++){
             desiredStr.put(t.charAt(i), desiredStr.getOrDefault(t.charAt(i), 0) + 1);
         }
         
+        // Flag To Check Whether The Inside Loops Run Everytime
         boolean flagFirst = true;
         boolean flagSecond = true;
         
+        // Desired String Count 
         int desiredCount = t.length();
+        
+        // Window String Count
         int findCount = 0;
         
+        // Indexes Of Front And Rear In The Loop
         int indexFront = -1;
         int indexRear = -1;
         
+        // Outer Loop To Run Till Both flagFirst And FlagSecond Is False
         while(flagFirst == true && flagSecond == true){
             
             flagFirst = false;
             flagSecond = false;
             
+            // Inputing Characters In The HashMap Till We Find All The Desired String Characters In It
             while(indexRear < s.length()-1 && findCount < desiredCount){
                 
                 indexRear++;
@@ -38,6 +50,7 @@ class Solution {
                 
             }
             
+            // Shortening The Window From The Front Until findCount Remains Equal To The desiredCount
             while(indexFront < indexRear && findCount == desiredCount){
                 
                 String possibleResult = s.substring(indexFront + 1, indexRear + 1);
@@ -64,6 +77,7 @@ class Solution {
             
         }
         
+        // Returning The Final Result
         return result;
         
     }
