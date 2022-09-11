a96de51ddc68d5e13c313c9a499374d450990813
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-11 12:10:46 +0530

    Create LongestIncreasingSubsequence.java

---
+++ LongestIncreasingSubsequence.java
@@ -0,0 +1,34 @@
+// TC: O(n^2) SC: O(n)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        
+        // To Store The Longest Subsequence Upto The Index Of Every Element
+        int dp[] = new int[nums.length];
+        
+        // To Store The Longest Subsequence Length
+        int maxCount = 0;
+        
+        // Tabulation
+        for(int i=0; i<nums.length; i++){
+            
+            int count = 0;
+            
+            // Checking All The Length Of Longest Subsequence Before The Number
+            for(int j=0; j<i; j++){
+                
+                // Cheecking For Strictly Increasing Subsequence
+                if(nums[j] < nums[i]){
+                    count = Math.max(count, dp[j]);
+                }
+            }
+            
+            // Adding Its Own Length After Merging With The Previous Maximum Length Subsequence
+            dp[i] = count + 1;
+            maxCount = Math.max(maxCount, dp[i]);
+            
+        }
+        
+        // Returning The Longest Subsequence Length
+        return maxCount;
+    }
+}
