fc6e0dd958b7274f5db8a4a3e469150244cd080e
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-12 12:13:58 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -8,30 +8,60 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/*
+Runtime: 2 ms, faster than 99.46% of Java online submissions for Add Two Numbers.
+Memory Usage: 42.5 MB, less than 89.13% of Java online submissions for Add Two Numbers.
+*/
+
+// Optimized Solution TC: O(n) SC: O(1)
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(0);
-        ListNode head = result;
+        
+        // Taking Pointers To Start Of Both The Lists
+        ListNode l1Pointer = l1;
+        ListNode l2Pointer = l2;
+        
+        // Creating A New Node For Storing The Sum
+        ListNode head = new ListNode(0);
+        
+        // Creating Pointer To The head
+        ListNode currentNode = head;
         int carry = 0;
-        while(l1 != null || l2 !=null || carry != 0){
-            int l1_val = (l1 != null)? l1.val : 0;
-            int l2_val = (l2 != null)? l2.val : 0;
+        
+        // Iterating Till All 3 Conditions Satisfy
+        while(l1Pointer != null || l2Pointer != null || carry !=0){
             
-            int res = l1_val + l2_val + carry;
+            // Taking Values As Input Till List Hits Null And If The Loop Still Continues To Run THen We Take 0 as Input
+            int l1Value = (l1Pointer != null) ? l1Pointer.val : 0;
+            int l2Value = (l2Pointer != null) ? l2Pointer.val : 0;
             
-            carry = res/10;
+            // Calculating Sum
+            int sum = l1Value + l2Value + carry;
             
-            int lastDigit = res%10;
+            // Calculating Last Digit
+            int lastDigit = sum%10;
             
-            ListNode digit = new ListNode(lastDigit);
-            head.next = digit;
-            head = head.next;
-            if(l1 != null)
-            l1 = l1.next;
-            if(l2 != null)
-            l2 = l2.next;
+            // Calculating Carry
+            carry = sum/10;
+            
+            // Storing Last Digit In A Node And Adding It To The Answer
+            ListNode lastDigitNode = new ListNode(lastDigit);
+            currentNode.next = lastDigitNode;
+            currentNode = currentNode.next;
+            
+            // Base Iterations
+            if(l1Pointer != null){
+                l1Pointer = l1Pointer.next;
+            }
+            if(l2Pointer != null){
+                l2Pointer = l2Pointer.next;
+            }
             
         }
-        return result.next;
+        
+        // Returning The Final Result
+        return head.next;
+        
     }
 }
