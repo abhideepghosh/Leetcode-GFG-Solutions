3fd1b10d40d6b4f913462b3c3c5902e202836b21
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-12 22:01:05 +0530

    Update SymmetricTree.java

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -15,17 +15,24 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+        // Calling isSymmetric Overloaded Function
         return isSymmetric(root, root);
     }
     
+    // Helper Function
     public boolean isSymmetric(TreeNode t1, TreeNode t2){
+        
+        // If All Conditions Satisfy And Both Nodes Are Null Then Return True
         if(t1 == null && t2 == null){
             return true;
         }
+        
+        // If All Conditions Satisfy But One Reaches Null Then Return False
         if(t1 == null || t2 == null){
             return false;
         }
-        return (t1.val == t2.val) && isSymmetric(t1.left,t2.right) && isSymmetric(t1.right,t2.left);
+        
+        // Calling Function With Left Right And Right Left Combo And Also Checking For Values
+        return t1.val == t2.val && isSymmetric(t1.left, t2.right) && isSymmetric(t1.right, t2.left);
     }
-    
 }
