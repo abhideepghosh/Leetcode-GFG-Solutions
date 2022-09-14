3b9312296cbf734c2545b31dbc1ced9421af80b0
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-14 14:24:03 +0530

    Update DeleteTheMiddleNodeOfALinkedList.java

--- DeleteTheMiddleNodeOfALinkedList.java
+++ DeleteTheMiddleNodeOfALinkedList.java
@@ -8,6 +8,42 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/*
+Runtime: 4 ms, faster than 96.55% of Java online submissions for Delete the Middle Node of a Linked List.
+Memory Usage: 63.3 MB, less than 93.17% of Java online submissions for Delete the Middle Node of a Linked List.
+*/
+// Optimized Solution TC: O(n) SC: O(1)
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        
+        // Edge Cases Covered
+        if(head.next == null){
+            return head.next;
+        }
+        
+        // StablePointer To Be Moved At Constant Speed
+        ListNode stablePointer = head;
+        
+        // UnstablePointer To Be Moved At Fast Speed (.next.next)
+        ListNode unstablePointer = head;
+        
+        // EntryPointer To Be Moved At Constant Speed But Will Stay Just Behind The StablePointer
+        ListNode entryPointer = null;
+        
+        while(unstablePointer != null && unstablePointer.next != null){
+            entryPointer = stablePointer;
+            stablePointer = stablePointer.next;
+            unstablePointer = unstablePointer.next.next;
+        }
+        
+        // Delete The Next Node
+        entryPointer.next = entryPointer.next.next;
+        
+        // Return Head
+        return head;
+    }
+}
+
 
 // Brute Force Solution : Calculate Size TC: O(n) SC: O(1)
 class Solution {
