4d656c3def720e930ffb57841a079787ba1461d5
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-16 11:26:31 +0530

    Update IncreasingTripletSubsequence.java

--- IncreasingTripletSubsequence.java
+++ IncreasingTripletSubsequence.java
@@ -1,17 +1,32 @@
+// Optimised Solution TC: O(n) SC: O(1)
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int i= Integer.MAX_VALUE;
-        int j = Integer.MAX_VALUE;
         
-        for(int in=0; in<nums.length; in++){
-            if(nums[in] <= i){
-                i = nums[in];
-            }else if(nums[in] <= j){
-                j = nums[in];
-            }else{
+        // For Finding The First Number
+        int firstNumber = Integer.MAX_VALUE;
+        
+        // For Finding The Second Number
+        int secondNumber = Integer.MAX_VALUE;
+        
+        
+        for(int num : nums){
+            
+            // All the Larger Numbers Will Be Stored In The First Number Till The Smallest First Number Is Found
+            if(num <= firstNumber){
+                firstNumber = num;
+            }
+            // Then We Move To The Next Greater Number
+            else if(num <= secondNumber){
+                secondNumber = num;
+            }
+            // If We Find A Third Greater Number, Rather Than Storing It, We Return True
+            else{
                 return true;
             }
         }
+        
+        // Else We Return False
         return false;
+        
     }
 }
