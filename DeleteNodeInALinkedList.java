ca772303dfe0db0eaf35fa6f197714b7bf5e19e2
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-17 11:49:30 +0530

    Update DeleteNodeInALinkedList.java

--- DeleteNodeInALinkedList.java
+++ DeleteNodeInALinkedList.java
@@ -6,9 +6,22 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Delete Node in a Linked List.
+Memory Usage: 41.5 MB, less than 98.03% of Java online submissions for Delete Node in a Linked List.
+*/
+
+// Optimised TC: O(1) SC: O(1) 
 class Solution {
+    
     public void deleteNode(ListNode node) {
-        node.val =node.next.val;
+              
+        // Making currentNode val equal to the nextNode val
+        node.val = node.next.val;
+        
+        // Making The cuurentNode's Next Pointer To Point At Next Node's next Pointer
         node.next = node.next.next;
     }
+    
 }
