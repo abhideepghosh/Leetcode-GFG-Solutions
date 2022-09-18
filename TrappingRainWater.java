237a864e296ad347cd9d4ebecbf27f344823f8bf
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-18 12:41:56 +0530

    Update TrappingRainWater.java

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -1,5 +1,67 @@
 /*
 Runtime: 1 ms, faster than 99.76% of Java online submissions for Trapping Rain Water.
+Memory Usage: 42.9 MB, less than 93.35% of Java online submissions for Trapping Rain Water.
+*/
+// Best Solution TC: O(N) SC: O(1)
+class Solution {
+    public int trap(int[] height) {
+        
+        // Declaring 2 Pointers At Start And End
+        int left = 0;
+        int right = height.length - 1;
+        
+        // For Storing Result
+        int result = 0;
+        
+        // For Storing Maximum From Left Side And Right Side In The Array While Traversing Using The Two Pointers 
+        int leftMax = 0;
+        int rightMax = 0;
+        
+        // Iterating Using 2 Pointers
+        while(left < right){
+            
+            // If Height At Left Pointer Is Smaller Than Height At Right Pointer
+            if(height[left] < height[right]){
+                
+                // We Check If Height At Left Is Larger Than leftMax
+                // If It Is, Then We Store The Value Of Height At leftMax
+                // Else, We Add The leftMax - Height Value To Amount Of Water
+                // We Do The Same For The Right Pointer
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                }
+                else{
+                    result += (leftMax - height[left]);
+                }
+                
+                left++;
+                
+            }
+            else{
+                
+                if(height[right] >= rightMax){
+                    rightMax = height[right];
+                }
+                else{
+                    result += (rightMax - height[right]);
+                }
+                
+                right--;
+                
+            }
+            
+        }
+        
+        // Returning Result
+        return result;
+        
+    }
+}
+
+
+
+/*
+Runtime: 1 ms, faster than 99.76% of Java online submissions for Trapping Rain Water.
 Memory Usage: 48.8 MB, less than 52.21% of Java online submissions for Trapping Rain Water.
 */
 // Optimized Solution TC: O(n) SC: O(n)
