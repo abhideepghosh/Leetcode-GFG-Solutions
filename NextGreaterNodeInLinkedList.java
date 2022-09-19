518183395c2ce0b885a38d8c7e1201ad416f6a2c
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-19 14:04:49 +0530

    Update NextGreaterNodeInLinkedList.java

--- NextGreaterNodeInLinkedList.java
+++ NextGreaterNodeInLinkedList.java
@@ -1,3 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 // TC: O(N*N) SC: O(N)
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
