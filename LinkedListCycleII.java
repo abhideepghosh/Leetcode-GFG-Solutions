d483bc0ead88ba5ea151d38605ad8b9c72fb849d
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-26 11:38:06 +0530

    Update LinkedListCycleII.java

--- LinkedListCycleII.java
+++ LinkedListCycleII.java
@@ -9,34 +9,59 @@
  *     }
  * }
  */
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Linked List Cycle II.
+Memory Usage: 42.3 MB, less than 99.03% of Java online submissions for Linked List Cycle II.
+*/
+// Optimized Solution TC: O(N) SC: O(1)
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         
-        ListNode constant = head;
-        ListNode fast = head;
-        ListNode entry = head;
-        
-        
-        if(head == null || head.next ==null){
+        // Handing Edge Cases
+        if(head == null || head.next == null){
             return null;
         }
         
+        // For Traversing The Linked List In Constant Speed
+        ListNode stablePointer = head;
         
-        while(fast.next!=null && fast.next.next!=null){
-            constant = constant.next;
-            fast = fast.next.next;
-            if(constant == fast){
-                while(constant != entry){
+        // For Traversing The Linked List In Fast Speed
+        ListNode unstablePointer = head;
+        
+        // For Finding The Cycle
+        ListNode entry = head;
+        
+        // Checking Fast Pointer -> In Case It Hits Null, We Return Null
+        while(unstablePointer.next != null && unstablePointer.next.next != null){
+            
+            // Traversing At Constant Speed
+            stablePointer = stablePointer.next;
+            
+            // Traversing At Faster Speed
+            unstablePointer = unstablePointer.next.next;
+            
+            // If The Stable And Unstable Pointer Meet At Any Point
+            // We Start A Node From Head At The Same Speed As The Stable Pointer
+            if(stablePointer == unstablePointer){
+                
+                // They Will Meet At The Cycle Node According To The Floyds Cycle Finding Algorithm
+                while(stablePointer != entry){
+                    
+                    // Traversing At Constant Speed
+                    stablePointer = stablePointer.next;
                     entry = entry.next;
-                    constant = constant.next;
+                    
                 }
-                return entry;
+                
+                // Returning The Node Where The Cycle Starts
+                return stablePointer;
+                
             }
+            
         }
         
+        // Returning Default Null
         return null;
-        
-
         
     }
 }
