bddbe82f7ffd40a9a2fb4e1f0016daa2c7277088
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-09-27 19:54:48 +0530

    Update DeleteMiddleElementOfAStack.java

--- DeleteMiddleElementOfAStack.java
+++ DeleteMiddleElementOfAStack.java
@@ -20,3 +20,38 @@ class Solution
         
     } 
 }
+
+
+
+// Recursive Method
+class Solution
+{
+    //Function to delete middle element of a stack.
+    public void deleteMid(Stack<Integer>s,int sizeOfStack){
+        // code here
+        
+        if(s.isEmpty()){
+            return;
+        }
+        
+        recursiveDeleteMid(s, sizeOfStack, 0);
+        
+    }
+    
+    public static void recursiveDeleteMid(Stack<Integer> stack, int sizeOfStack, int count){
+        
+        if(count >= sizeOfStack/2){
+            stack.pop();
+            return;
+        }
+        
+        int currentValue = stack.peek();
+        stack.pop();
+        
+        recursiveDeleteMid(stack, sizeOfStack, count + 1);
+        
+        stack.push(currentValue);
+        
+    }
+    
+}
