f77fc61102b701ad454179a6339927ef88fd70ba
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-10-15 11:20:01 +0530

    Update ClimbingStairs.java

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,18 +1,29 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Climbing Stairs.
+Memory Usage: 38.7 MB, less than 97.94% of Java online submissions for Climbing Stairs.
+*/
+// Tabulation : TC: O(N) SC: O(N)
 class Solution {
     public int climbStairs(int n) {
         
-        if(n==0){
-            return 0;
+        // Handling Edge Cases
+        if(n == 1){
+            return 1;
         }
         
-        int[] dp = new int[n+1];
+        // Standard DP Template
+        int[] dp = new int[n + 1];
+        
+        // Assigning The First 2 Elements Of DP
         dp[0] = 1;
         dp[1] = 1;
         
-        for(int i=2; i<=n; i++){
+        // Calculating Current Element Using The Previous 2 Elementss
+        for(int i = 2; i<dp.length; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
         
+        // Returning The Result Stored In dp[n]
         return dp[n];
         
     }
