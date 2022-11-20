dd1ef0042b92e955c7c4735d42700f3fbfd704c3
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-11-20 16:32:39 +0530

    Update PowerOfThree.java

--- PowerOfThree.java
+++ PowerOfThree.java
@@ -1,15 +1,23 @@
 // Optimised Solution
+/*
+Runtime: 15 ms, faster than 95.97% of Java online submissions for Power of Three.
+Memory Usage: 42.5 MB, less than 83.84% of Java online submissions for Power of Three.
+*/
 class Solution {
     public boolean isPowerOfThree(int n) {
+        
         if(n < 1){
             return false;
         }
         
-        while(n%3 == 0){
-            n /= 3;
+        while(n % 3 == 0){
+            
+            n = n / 3;
+            
         }
         
         return n == 1;
+        
     }
 }
 
