eecb9a6c452f367a620dbb3adf6ff01cccfd8e81
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-11-21 11:36:42 +0530

    Create DivisorGame.java

---
+++ DivisorGame.java
@@ -0,0 +1,10 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Divisor Game.
+Memory Usage: 38.7 MB, less than 99.07% of Java online submissions for Divisor Game.
+*/
+// TC: O(1) SC: O(1)
+class Solution {
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+}
