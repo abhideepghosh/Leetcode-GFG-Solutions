e78da916360f105e94e23507ef0cd3821c429f7c
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-11-21 11:38:02 +0530

    Update DivisorGame.java

--- DivisorGame.java
+++ DivisorGame.java
@@ -8,3 +8,15 @@ class Solution {
         return n % 2 == 0;
     }
 }
+
+
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Divisor Game.
+Memory Usage: 38.8 MB, less than 94.89% of Java online submissions for Divisor Game.
+*/
+// TC: O(1) SC: O(1)
+class Solution {
+    public boolean divisorGame(int N) {
+        return (N & 1) == 0;   
+    }
+}
