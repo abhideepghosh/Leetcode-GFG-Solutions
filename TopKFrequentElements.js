3ee38cc4e2ae4948060594de54a0f5fd019246df
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-01-05 14:25:36 +0530

    Update TopKFrequentElements.js

--- TopKFrequentElements.js
+++ TopKFrequentElements.js
@@ -1,21 +1,20 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
- */
 var topKFrequent = function(nums, k) {
     
-    // For Keeping Element As Key And Frequency Of The Element As Value
-    const pair = {};
-
-    // Creating A Frequency Map Of Elements
+    const freqMap = new Map();
     nums.forEach((num) => {
-       pair[num] = pair[num] ? pair[num] + 1 : 1;
-    }); 
-
-    // Sorted Based Off On Values(Frequencies) Of The Keys(Elements)
-    const sorted = Object.keys(pair).sort((a,b) => {return pair[b]-pair[a]});
+        freqMap.set(num, freqMap.has(num) ? freqMap.get(num) + 1 : 1);
+    });
     
-    // Returning The First K Elements
-    return sorted.splice(0,k);
+    const freqArray = [...freqMap].map((key) => key);
+
+    freqArray.sort((a,b) => b[1] - a[1]);
+    
+    const result = [];
+    
+    for(let i=0; i<k; i++){
+        result.push(freqArray[i][0]);
+    }
+    
+    return result;
+    
 };
