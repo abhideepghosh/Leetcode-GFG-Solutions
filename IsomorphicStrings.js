defe4465f7e454b5b279d41fcb30c89c149bcf00
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-01-24 22:26:49 +0530

    Create IsomorphicStrings.js

---
+++ IsomorphicStrings.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if(s.length !== t.length) return false;
+    const map = new Map();
+    const set = new Set();
+
+    for(let i=0; i<s.length; i++) {
+        if(map.has(s[i]) && map.get(s[i]) !== t[i]) return false;
+        if(!map.has(s[i]) && set.has(t[i])) return false;
+        map.set(s[i], t[i]);
+        set.add(t[i]);
+    }
+    return true;
+};
