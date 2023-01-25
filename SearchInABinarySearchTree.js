b7c51a56c247c2c438cbc2613c9c473022da6d35
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-01-25 13:45:31 +0530

    Create SearchInABinarySearchTree.js

---
+++ SearchInABinarySearchTree.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if(!root) return null;
+    if(root.val === val) return root;
+    else if(root.val > val) return searchBST(root.left, val);
+    else return searchBST(root.right, val);
+};
