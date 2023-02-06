6d89d1054496e58e5822942c00617e0c8668f3c0
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-02-06 12:09:31 +0530

    Update NextGreaterElementI.js

--- NextGreaterElementI.js
+++ NextGreaterElementI.js
@@ -10,28 +10,16 @@
 
 var nextGreaterElement = function(nums1, nums2) {
     const stack = [];
-    const map = new Map();
-
-    const peek = () => {
-        return stack[stack.length - 1];
-    }
-
-    const mapFill = (num) => {
-        while(stack.length !== 0 && peek() < num) {
-            map.set(stack.pop(), num);
-        }
+     const map = new Map();
+        
+    const peek = () => stack[stack.length - 1];
+        
+    nums2.forEach((num) => {
+        while(stack.length !== 0 && peek() < num) map.set(stack.pop(), num);
         stack.push(num);
-    }
-
-    const generateArray = (num) => {
-        if(map.has(num)) return map.get(num);
-        else return -1;
-    }
-
-    nums2.forEach(mapFill);
-
-    const result = nums1.map(generateArray);
-
+    });
+        
+    const result = nums1.map((num) => map.has(num) ? map.get(num) : -1);
     return result;
 
 };
