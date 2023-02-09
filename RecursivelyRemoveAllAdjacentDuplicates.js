661f516b6280d29df1cf3778e87d59b47cc92390
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-02-09 12:12:39 +0530

    Create RecursivelyRemoveAllAdjacentDuplicates.js

---
+++ RecursivelyRemoveAllAdjacentDuplicates.js
@@ -0,0 +1,38 @@
+//User function Template for javascript
+
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+class Solution {
+  
+  remove(s) {
+      let result = '';
+      let i=0;
+      const n = s.length
+      while(i < n) {
+          if(i < n - 1 && s[i] === s[i + 1]) {
+              while(i < n - 1 && s[i] === s[i + 1]) i++;
+          }
+          else {
+              result += s[i];
+          }
+          i++;
+      }
+      return result;
+  }
+  
+  rremove(s) {
+    //code here
+    let result = '';
+    while(s.length !== result.length) {
+        result = s;
+        s = this.remove(s);
+    }
+    return result;
+    
+  }
+}
