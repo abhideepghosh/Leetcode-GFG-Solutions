5d6f8169ed734977c35d2bd11a6d9422973212e7
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-02-09 10:42:12 +0530

    Update FindFirstAndLastPositionOfElementInSortedArray.js

--- FindFirstAndLastPositionOfElementInSortedArray.js
+++ FindFirstAndLastPositionOfElementInSortedArray.js
@@ -3,30 +3,29 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
-    let midPosition = binarySearch(nums, 0, nums.length - 1, target); 
-    if(midPosition === -1) return [-1, -1];
-    let start, end;
-    let startPos = midPosition, endPos = midPosition;
-    while(startPos !== -1){
-        start = startPos;
-        startPos = binarySearch(nums, 0, startPos - 1, target);
-    }
-    while(endPos !== -1){
-        end = endPos;
-        endPos = binarySearch(nums, endPos+1, nums.length - 1, target);
-    }
-    return [start, end];
 
-};
-
-const binarySearch = (nums, start, end, target) => {
-    while(start <= end){
-        let mid = Math.floor(start + (end - start) / 2);
+const search = function(nums, target, start, end) {
+    while(start <= end) {
+        const mid = start + Math.floor((end - start) / 2);
         if(nums[mid] === target) return mid;
-        if(nums[mid] < target) start = mid + 1;
-        if(nums[mid] > target) end = mid - 1;
+        else if(nums[mid] < target) start = mid + 1;
+        else end = mid - 1;
     }
     return -1;
+};
 
-}
+var searchRange = function(nums, target) {
+    let startPosition = search(nums, target, 0 , nums.length - 1);
+    if(startPosition === -1) return [-1, -1];
+    let endPosition = startPosition;
+    let start, end;
+    while (startPosition !== - 1) {
+        start = startPosition;
+        startPosition = search(nums,target, 0, startPosition - 1);
+    }
+    while(endPosition !== - 1) {
+        end = endPosition;
+        endPosition = search(nums, target, endPosition + 1, nums.length - 1);
+    }
+    return [start, end];
+};
