e33ea8c541ecbdb6ca84b870cd2bcd54c78476f1
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-02-09 12:14:21 +0530

    Update RecursivelyRemoveAllAdjacentDuplicates.js

--- RecursivelyRemoveAllAdjacentDuplicates.js
+++ RecursivelyRemoveAllAdjacentDuplicates.js
@@ -8,7 +8,7 @@ class Solution {
   remove(s) {
       let result = '';
       let i=0;
-      const n = s.length
+      const n = s.length;
       while(i < n) {
           if(i < n - 1 && s[i] === s[i + 1]) {
               while(i < n - 1 && s[i] === s[i + 1]) i++;
