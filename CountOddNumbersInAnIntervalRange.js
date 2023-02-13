544941f244642e22c62d432b7e7d4ff95027e0f5
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-02-13 11:17:05 +0530

    Update CountOddNumbersInAnIntervalRange.js

--- CountOddNumbersInAnIntervalRange.js
+++ CountOddNumbersInAnIntervalRange.js
@@ -3,8 +3,14 @@
  * @param {number} high
  * @return {number}
  */
+// Approach 1
 var countOdds = function(low, high) {
     low = low % 2 === 0 ? low + 1 : low;
     high = high % 2 === 0 ? high -1 : high;
     return low > high ? 0 : Math.floor((high - low) / 2) + 1;
+};
+
+// Approach 2
+var countOdds = function(low, high) {
+    return Math.floor((high - low)/2) + (low%2 || high%2 ? 1 : 0);
 };
