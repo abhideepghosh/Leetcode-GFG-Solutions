3989fa442fc7a82bc099752c9de040738c06e14a
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-03-01 12:29:18 +0530

    Update SpiralMatrix.js

--- SpiralMatrix.js
+++ SpiralMatrix.js
@@ -3,32 +3,29 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
+    let rowStart = 0, rowEnd = matrix.length - 1;
+    let colStart = 0, colEnd = matrix[0].length - 1;
     const result = [];
-    let rowStart = 0, colStart = 0;
-    let rowEnd = matrix.length - 1;
-    let colEnd = matrix[0].length - 1;
-
-    while(rowStart <= rowEnd && colStart <= colEnd){
-        for(let i=colStart; i<=colEnd; i++){
-            result.push(matrix[rowStart][i]);
-        }
+    while(rowStart <= rowEnd && colStart <= colEnd) {
+        // Moving East 
+        for(let i=colStart; i <=colEnd; i++) result.push(matrix[rowStart][i]);
         rowStart++;
-        for(let i=rowStart; i<=rowEnd; i++){
-            result.push(matrix[i][colEnd]);
-        }
+
+        // Moving South
+        for(let i=rowStart; i <=rowEnd; i++) result.push(matrix[i][colEnd]);
         colEnd--;
-        if(rowStart <= rowEnd) {
-            for(let i=colEnd; i>=colStart; i--) {
-                result.push(matrix[rowEnd][i]);
-            }
-            rowEnd--;
-        }
-        if(colStart <= colEnd){
-            for(let i=rowEnd; i>=rowStart; i--){
-                result.push(matrix[i][colStart]);
-            }
-            colStart++;
-        }
+
+        if(rowStart > rowEnd || colStart > colEnd) break;
+
+        // Moving West
+        for(let i=colEnd; i >= colStart; i--) result.push(matrix[rowEnd][i]);
+        rowEnd--;
+
+        if(rowStart > rowEnd || colStart > colEnd) break;
+
+        // Moving North
+        for(let i=rowEnd; i >= rowStart; i--) result.push(matrix[i][colStart]);
+        colStart++;
     }
     return result;
 };
