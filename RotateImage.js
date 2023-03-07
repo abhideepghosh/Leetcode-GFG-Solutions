e23877c8b74080718387712d7a533977c625c2bf
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-03-07 10:03:20 +0530

    Create RotateImage.js

---
+++ RotateImage.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+
+const swap = (nums, i, j) => {
+    const temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+const reverse = (nums) => {
+    let start = 0;
+    let end = nums.length - 1;
+    while(start < end) {
+        swap(nums, start, end);
+        start++;
+        end--;
+    }
+}
+
+var rotate = function(matrix) {
+    for(let i=0; i<matrix.length; i++) {
+        for(let j=i; j<matrix[0].length; j++) {
+            const temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    for(let i=0; i<matrix.length; i++) {
+        reverse(matrix[i]);
+    }
+};
