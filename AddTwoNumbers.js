d288d623861eafd075596c80fe228abcfdb11fdf
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-03-07 09:39:18 +0530

    Update AddTwoNumbers.js

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -14,21 +14,23 @@ var addTwoNumbers = function(l1, l2) {
     let result = new ListNode(0);
     let resultPointer = result;
     let carry = 0;
-    while(l1 || l2 || carry !== 0) {
-       
-        const l1Value = l1 ? l1.val : 0;
-        const l2Value = l2 ? l2.val : 0;
-        
-        const sum = l1Value + l2Value + carry;
-        
+    
+    while(l1 || l2 || carry) {
+        const l1Val = l1 ? l1.val : 0;
+        const l2Val = l2 ? l2.val : 0;
+
+        const sum = l1Val + l2Val + carry;
+
         resultPointer.next = new ListNode(Math.floor(sum % 10));
-        resultPointer = resultPointer.next;
-        
         carry = Math.floor(sum / 10);
-        
-        l1 ? l1=l1.next : null;
-        l2 ? l2=l2.next : null;
+
+        resultPointer = resultPointer.next;
+
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
 
     }
+
     return result.next;
+
 };
