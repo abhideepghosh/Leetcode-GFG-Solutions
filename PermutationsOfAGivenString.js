2e8c44da5ffc2202c252d5b84a6dc824a2309663
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-03-11 11:49:26 +0530

    Update PermutationsOfAGivenString.js

--- PermutationsOfAGivenString.js
+++ PermutationsOfAGivenString.js
@@ -7,6 +7,45 @@ class Solution {
     find_permutation(S){
          //code here
          
+         const permutations = (arr, result, store, freq) => {
+             if(store.length === arr.length) {
+                 result.add(store);
+                 return;
+             }
+             for(let i=0; i<arr.length; i++) {
+                 if(!freq[i]) {
+                     freq[i] = true;
+                     store += arr[i];
+                     permutations(arr, result, store, freq);
+                     store = store.slice(0, -1);
+                     freq[i] = false;
+                 }
+             }
+         }
+         
+         const arr = [...S];
+         const result = new Set();
+         let store = '';
+         const freq = [];
+         
+         permutations(arr, result, store, freq);
+         return [...result].sort();
+         
+    }
+}
+
+
+
+
+/**
+ * @param {string} S
+ * @return {string[]}
+ */
+
+class Solution {
+    find_permutation(S){
+         //code here
+         
          const permutation = (arr, store, result, freq) => {
              if(store.length === arr.length) {
                  result.add(store.join(''));
