9fec622ce16f64be52800bc9c8c179c2f4fa985c
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-03-14 11:39:27 +0530

    Update 0-1KnapsackProblem.js

--- 0-1KnapsackProblem.js
+++ 0-1KnapsackProblem.js
@@ -1,27 +1,28 @@
+/**
+ * @param {number} W
+ * @param {number[]} wt
+ * @param {number[]} val
+ * @param {number} n
+ * @returns {number}
+*/
+
 class Solution 
 {
     //Function to return max value that can be put in knapsack of capacity W.
     knapSack(W, wt, val, n)
     { 
        // code here
-       const dp = new Array(val.length + 1);
-       for(let i=0; i<dp.length; i++){
-           for(let j=0; j<W + 1; j++){
-               dp[i] = new Array(W + 1);
-           }
+       const dp = [];
+       for(let i=0; i<val.length + 1; i++) {
+           dp[i] = new Array(W + 1).fill(0);
        }
-       for(let j=0; j<dp[0].length; j++){
-           dp[0][j] = 0;
-       }
-       for(let i=0; i<dp.length; i++){
-           dp[i][0] = 0;
-       }
-       for(let i=1; i<dp.length; i++){
-           for(let j=1; j<dp[0].length; j++){
+       for(let i=1; i<dp.length; i++) {
+           for(let j=1; j<dp[i].length; j++) {
                if(j >= wt[i - 1]){
-                   dp[i][j] = Math.max(dp[i-1][j], val[i-1] + dp[i-1][j - wt[i-1]]);
-               }else{
-                   dp[i][j] = dp[i-1][j];
+                   dp[i][j] = Math.max(dp[i-1][j - wt[i - 1]] + val[i - 1], dp[i - 1][j]);
+               }
+               else {
+                   dp[i][j] = dp[i - 1][j];
                }
            }
        }
