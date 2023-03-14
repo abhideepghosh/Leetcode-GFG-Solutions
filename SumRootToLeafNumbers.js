95f133d1351f5fcdf1e8904bb28565ba2f3900bc
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-03-14 10:52:24 +0530

    Update SumRootToLeafNumbers.js

--- SumRootToLeafNumbers.js
+++ SumRootToLeafNumbers.js
@@ -11,6 +11,39 @@
  * @return {number}
  */
 
+
+// Optimized Solution TC: O(N) SC: O(N)
+var sumNumbers = function(root) {
+    if(!root.left && !root.right) return root.val;
+    let result = 0;
+    const createNum = (root, str) => {
+        if(!root.left && !root.right) {
+            result += Number(str + `${root.val}`);
+            return;
+        }
+        if(root.left) createNum(root.left, str + `${root.val}`);
+        if(root.right) createNum(root.right, str + `${root.val}`);
+    }
+    
+    createNum(root, '');
+    return result;
+};
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// Brute Force Solution TC: O(N) SC: O(N)
 const createNum = (root, result, str) => {
     if(!root.left && !root.right) {
         result.push(str + `${root.val}`);
