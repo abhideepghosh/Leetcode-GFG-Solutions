67789575802793981c434c483b54755a465d932d
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-04-20 15:11:58 +0530

    Update InvertBinaryTree.js

--- InvertBinaryTree.js
+++ InvertBinaryTree.js
@@ -1,13 +1,19 @@
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
  * @param {TreeNode} root
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    if(!root) return root;
-
-    let left = invertTree(root.left);
-    let right = invertTree(root.right);
-
+    if(!root) return null;
+    const left = invertTree(root.left);
+    const right = invertTree(root.right);
     root.left = right;
     root.right = left;
 
