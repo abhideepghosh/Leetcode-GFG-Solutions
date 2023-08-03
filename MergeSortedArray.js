e38ab8e060f770960c028af537d1fc1151baca72
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-08-04 00:12:02 +0530

    Create MergeSortedArray.js

---
+++ MergeSortedArray.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    m--;
+    n--;
+    for(let i=nums1.length - 1; i >=0; i--) {
+        if(n < 0) return;
+        if((m < 0 && i >= 0) || (nums1[m] < nums2[n])) {
+            nums1[i] = nums2[n];
+            n--;
+        }
+        else {
+            nums1[i] = nums1[m];
+            m--;
+        }
+    }
+};
