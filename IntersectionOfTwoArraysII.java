94fdff1c10e462a79d873acf6009ccaaa02ddc93
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-09-10 00:00:00 +0530

    Update IntersectionOfTwoArraysII.java

--- IntersectionOfTwoArraysII.java
+++ IntersectionOfTwoArraysII.java
@@ -18,25 +18,8 @@ class Solution {
     }
 }
 
-// Approach 2: Another Optimized But Scalable Solution TC: O(N) SC: O(1)
-// TC: O(N) SC: O(N)
-var intersect = function(nums1, nums2) {
-    const map = new Map();
-    nums1.forEach(num => {
-        map.set(num, map.has(num) ? map.get(num) + 1 : 1);
-    });
-    const result = [];
-    nums2.forEach(num => {
-        if(map.has(num)) {
-            result.push(num);
-            map.get(num) > 1 ? map.set(num, map.get(num) - 1) : map.delete(num);
-        }
-    });
-    return result;
-};
 
-
-// //      Approach 3: More Optimized Than The Brute Force Solution:
+// //      Approach 2: More Optimized Than The Brute Force Solution:
 // class Solution {
 //     public int[] intersect(int[] nums1, int[] nums2) {
         
