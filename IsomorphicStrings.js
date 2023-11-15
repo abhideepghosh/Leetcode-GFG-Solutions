20de986bf9d85f4cfd3efd09537fc921afeef3af
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-11-15 17:20:44 +0530

    Update IsomorphicStrings.js

--- IsomorphicStrings.js
+++ IsomorphicStrings.js
@@ -3,13 +3,13 @@
  * @param {string} t
  * @return {boolean}
  */
+// TC: O(N) SC: O(N)
 var isIsomorphic = function(s, t) {
     if(s.length !== t.length) return false;
     const map = new Map();
     const set = new Set();
-
     for(let i=0; i<s.length; i++) {
-        if(map.has(s[i]) && map.get(s[i]) !== t[i]) return false;
+        if(map.has(s[i]) && map.get(s[i]) !== t[i]) return false;      
         if(!map.has(s[i]) && set.has(t[i])) return false;
         map.set(s[i], t[i]);
         set.add(t[i]);
