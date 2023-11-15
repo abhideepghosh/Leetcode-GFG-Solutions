5f7d7ecc305d6183b2409cc33e7b714c0c138908
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-11-15 15:52:22 +0530

    Create SetMatrixZeroes.js

---
+++ SetMatrixZeroes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+// TC: O(N^2) SC: O(1)
+var setZeroes = function(matrix) {
+    let isColumnOne = true, rows = matrix.length, cols = matrix[0].length;
+    for(let i=0; i<rows; i++) {
+        if(matrix[i][0] === 0) isColumnOne = false;
+        for(let j=1; j<cols; j++) {
+            if(matrix[i][j] === 0) matrix[i][0] = matrix[0][j] = 0;
+        }
+    }
+    for(let i=rows - 1; i>=0; i--) {
+        for(let j=cols - 1; j>=1; j--) {
+            if(matrix[i][0] === 0 || matrix[0][j] === 0) matrix[i][j] = 0;
+        }
+        if(!isColumnOne) matrix[i][0] = 0;
+    }
+};
