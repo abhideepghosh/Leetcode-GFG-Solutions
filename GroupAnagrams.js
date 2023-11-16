99138ba90254b64799e1189c285304e2eed990ee
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-11-16 17:45:37 +0530

    Update GroupAnagrams.js

--- GroupAnagrams.js
+++ GroupAnagrams.js
@@ -2,24 +2,23 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+ // TC: O(N^2) SC: O(N)
 var groupAnagrams = function(strs) {
     const words = strs.map((word) => [...word].sort().join(''));
-    const indexSet = new Set();
-    
     const result = [];
-
-    words.forEach((word, i) => {
-        if(!indexSet.has(i)){
-            indexSet.add(i);
-            const list = [strs[i]];
-            for(let j=i+1; j<words.length; j++){
-                if(!indexSet.has(j) && word === words[j]){
-                    list.push(strs[j]);
-                    indexSet.add(j);
+    const set = new Set();
+    for(let i=0; i<words.length; i++) {
+        if(!set.has(i)) {
+            const anagrams = [strs[i]];
+            set.add(i);
+            for(let j=i+1; j<words.length; j++) {
+                if(words[i] === words[j]) {
+                    anagrams.push(strs[j]);
+                    set.add(j);
                 }
             }
-            result.push(list);
+            result.push(anagrams);
         }
-    });
+    }
     return result;
 };
