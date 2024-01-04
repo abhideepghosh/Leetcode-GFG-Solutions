b7a0bd8bd8ee2829c114cbf4de7840c0f8cc1942
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-01-04 18:11:11 +0530

    Update SortAnArrayOf0s1sAnd2s.js

--- SortAnArrayOf0s1sAnd2s.js
+++ SortAnArrayOf0s1sAnd2s.js
@@ -5,6 +5,35 @@
  * @returns {number[]}
 */
 
+// TC: O(N) SC: O(1) Optimized Solution
+class Solution {
+    
+    sort012(arr, N)
+    {
+        //your code here
+        const swap = (arr, i, j) => {
+            const temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        let zeroIdx = 0, idx = 0, twoIdx = N - 1;
+        while(idx <= twoIdx) {
+            if(arr[idx] === 1) idx++;
+            else if(arr[idx] === 2) {
+                swap(arr, idx, twoIdx);
+                twoIdx--;
+            }
+            else {
+                swap(arr, zeroIdx, idx);
+                zeroIdx++;
+                idx++;
+            }
+        }
+        return arr;
+    }
+}
+
+// TC: O(N) SC: O(1) Optimal Solution
 class Solution {
     
     swap(arr, i, j) {
