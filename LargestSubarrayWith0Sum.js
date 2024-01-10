0bb5137a81d6f48c3966231895836274286caf76
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-01-10 13:24:25 +0530

    Update LargestSubarrayWith0Sum.js

--- LargestSubarrayWith0Sum.js
+++ LargestSubarrayWith0Sum.js
@@ -1,24 +1,18 @@
-
 /**
  * @param {Number[]} arr
  * @param {Number} n
  * @returns {Number}
  */
-
+// TC: O(N) SC: O(N)
 class Solution {
     maxLen(arr,n){
         //code here
-        let sum = 0;
+        let maxLength = 0, sum = 0;
         const map = new Map([[sum, -1]]);
-        let max = 0;
-        arr.forEach((num, i)=> {
+        arr.forEach((num, i) => {
             sum += num;
-            if(!map.has(sum)) map.set(sum, i);
-            else {
-                const len = i - map.get(sum);
-                max = Math.max(max, len);
-            }
+            !map.has(sum) ? map.set(sum, i) : maxLength = Math.max(maxLength, i - map.get(sum));    
         });
-        return max;
+        return maxLength;
     }
 }
