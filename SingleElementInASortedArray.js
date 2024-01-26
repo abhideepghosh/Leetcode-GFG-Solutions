11b60e45932f36badb62671794f62c41176f48c9
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-01-26 21:02:15 +0530

    Update SingleElementInASortedArray.js

--- SingleElementInASortedArray.js
+++ SingleElementInASortedArray.js
@@ -2,15 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
+// TC: O(logN) SC: O(1)
 var singleNonDuplicate = function(nums) {
-    if(nums.length === 1) return nums[0];
-    let start = 0;
-    let end = nums.length - 1;
-    while(start <= end){
-        if(nums[start] === nums[start + 1]) start +=2;
+    let start = 0, end = nums.length - 1;
+    while(start <= end) {
+        if(nums[start] === nums[start + 1]) start += 2;
         else return nums[start];
-        if(nums[end] === nums[end-1]) end -=2;
+        if(nums[end] === nums[end - 1]) end -= 2;
         else return nums[end];
+    }
+    return -1;
+};
+
+
+// TC: O(N/2) SC: O(1)
+var singleNonDuplicate = function(nums) {
+    for(let i= 0; i<nums.length; i+=2) {
+        if(i === nums.length - 1) return nums[i];
+        else if(nums[i] !== nums[i + 1]) return nums[i];
     }
     return -1;
 };
