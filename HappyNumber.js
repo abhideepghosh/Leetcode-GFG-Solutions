3145d2cb9beaccc16cd578016668f7cc4a938c40
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-02-04 16:04:07 +0530

    Update HappyNumber.js

--- HappyNumber.js
+++ HappyNumber.js
@@ -2,18 +2,34 @@
  * @param {number} n
  * @return {boolean}
  */
-
-const squareOfDigits = (n) => {
-    return [...`${n}`].reduce((acc, curr) => acc + (Number(curr) ** 2), 0);
-}
-
 var isHappy = function(n) {
+    const breakAndSquare = (num) => {
+        let sum = 0;
+        while(num >= 1) {
+            sum += (num % 10) ** 2;
+            num = Math.floor(num / 10);
+        }
+        return sum;
+    }
     const set = new Set();
-    
     while(!set.has(n)) {
+        if(n === 1) return true;
+        else{
+            set.add(n);
+            n = breakAndSquare(n);
+        }
+    }
+    return false;
+};
+
+// Alternate Solution
+var isHappy = function(n) {
+    const breakAndSquare = (num) => [...`${num}`].reduce((acc, curr) => acc + Number(curr % 10) ** 2, 0);
+    const set = new Set();
+    while(!set.has(n)) {
+        if(n === 1) return true;
         set.add(n);
-        n = squareOfDigits(n);
-        if(n === 1) return true; 
+        n = breakAndSquare(n);
     }
     return false;
 };
