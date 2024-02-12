90d5729b079ce3202cd882e2473ab60eb5994b24
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-02-12 22:03:23 +0530

    Update KthLargestElement.js

--- KthLargestElement.js
+++ KthLargestElement.js
@@ -1,3 +1,102 @@
+// Solved Using Heaps
+// TC: O(NlogN) SC: O(N)
+class MaxHeap {
+    constructor(nums = []) {
+        this.heap = [];
+        nums.forEach(num => this.insert(num));
+    }
+
+    getParentIndex(index) {
+        return Math.floor((index - 1) / 2);
+    }
+
+    getLeftChildIndex(index) {
+        return 2 * index + 1;
+    }
+
+    getRightChildIndex(index) {
+        return 2 * index + 2;
+    }
+
+    swap(index1, index2) {
+        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.heapifyUp();
+    }
+
+    heapifyUp() {
+        let currentIndex = this.heap.length - 1;
+        while (currentIndex > 0) {
+            const parentIndex = this.getParentIndex(currentIndex);
+            if (this.heap[currentIndex] > this.heap[parentIndex]) {
+                this.swap(currentIndex, parentIndex);
+                currentIndex = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    extractMax() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+        const maxValue = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown();
+        return maxValue;
+    }
+
+    heapifyDown() {
+        let currentIndex = 0;
+        while (this.getLeftChildIndex(currentIndex) < this.heap.length) {
+            const leftChildIndex = this.getLeftChildIndex(currentIndex);
+            const rightChildIndex = this.getRightChildIndex(currentIndex);
+            let largerChildIndex = leftChildIndex;
+            if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] > this.heap[leftChildIndex]) {
+                largerChildIndex = rightChildIndex;
+            }
+            if (this.heap[currentIndex] < this.heap[largerChildIndex]) {
+                this.swap(currentIndex, largerChildIndex);
+                currentIndex = largerChildIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+}
+
+var findKthLargest = function(nums, k) {
+    const maxHeap = new MaxHeap(nums);
+    for (let i = 0; i < k - 1; i++) {
+        maxHeap.extractMax();
+    }
+    return maxHeap.peek();
+};
+
+
+
+
+
+// Solved Using Maps
 /**
  * @param {number[]} nums
  * @param {number} k
