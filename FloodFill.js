48bb0a09f26fb15c0ae8402c2bdfe823a06d95c6
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-03-14 12:54:50 +0530

    Update FloodFill.js

--- FloodFill.js
+++ FloodFill.js
@@ -5,37 +5,19 @@
  * @param {number} color
  * @return {number[][]}
  */
-
-const floodItUp = (image, sr, sc, originalColor, color) => {
-    if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] !== originalColor) {
-        return;
-    }
-    image[sr][sc] = color;
-    const outputs = [
-        {
-            sr: sr + 1,
-            sc: sc
-        },
-        {
-            sr: sr - 1,
-            sc: sc
-        },
-        {
-            sr: sr,
-            sc: sc + 1
-        },
-        {
-            sr: sr,
-            sc: sc - 1
-        },
-    
-    ];
-    outputs.forEach((output) => floodItUp(image, output.sr, output.sc, originalColor, color));
-}
-
 var floodFill = function(image, sr, sc, color) {
     if(image[sr][sc] === color) return image;
-    originalColor = image[sr][sc];
-    floodItUp(image, sr, sc, originalColor, color);
+    const flood = (image, sr, sc, originalColor, color) => {
+        if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] !== originalColor) {
+            return;
+        }
+        image[sr][sc] = color;
+        flood(image, sr + 1, sc, originalColor, color);
+        flood(image, sr - 1, sc, originalColor, color);
+        flood(image, sr, sc + 1, originalColor, color);
+        flood(image, sr, sc - 1, originalColor, color);
+    }
+    const originalColor = image[sr][sc];
+    flood(image, sr, sc, originalColor, color);
     return image;
 };
