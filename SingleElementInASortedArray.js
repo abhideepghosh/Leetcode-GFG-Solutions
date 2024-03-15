b611cd157496caa9c5a33ccd1b13f3f64017b090
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-03-15 18:14:53 +0530

    Update SingleElementInASortedArray.js

--- SingleElementInASortedArray.js
+++ SingleElementInASortedArray.js
@@ -2,24 +2,45 @@
  * @param {number[]} nums
  * @return {number}
  */
-// TC: O(logN) SC: O(1)
+// TC: O(logN) SC: O(1) Optimized Solution
 var singleNonDuplicate = function(nums) {
     let start = 0, end = nums.length - 1;
     while(start <= end) {
-        if(nums[start] === nums[start + 1]) start += 2;
+        if(nums[start] === nums[start + 1]) {
+            start += 2;
+        }
         else return nums[start];
-        if(nums[end] === nums[end - 1]) end -= 2;
+        if(nums[end] === nums[end - 1]) {
+            end -= 2;
+        }
         else return nums[end];
     }
     return -1;
 };
 
-
-// TC: O(N/2) SC: O(1)
+// TC: O(logN) SC: O(1) Optimized  Solution (Binary Search)
 var singleNonDuplicate = function(nums) {
-    for(let i= 0; i<nums.length; i+=2) {
-        if(i === nums.length - 1) return nums[i];
-        else if(nums[i] !== nums[i + 1]) return nums[i];
+    let start = 0, end = nums.length - 1;
+    while(start < end) {
+        const mid = Math.floor(start + (end - start) / 2);
+        if(nums[mid] !== nums[mid + 1] && nums[mid] !== nums[mid - 1]) return nums[mid];
+        const isLeftEven = Math.floor((mid - start) % 2);
+        if(isLeftEven) {
+            if(nums[mid] === nums[mid - 1]) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        else {
+            if(nums[mid] === nums[mid + 1]) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
     }
-    return -1;
+    return nums[start];
 };
