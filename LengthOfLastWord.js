fa646466f728ffd3bec2ca3ff839461a2ecbcfaf
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-04-01 11:27:46 +0530

    Update LengthOfLastWord.js

--- LengthOfLastWord.js
+++ LengthOfLastWord.js
@@ -2,6 +2,25 @@
  * @param {string} s
  * @return {number}
  */
+// TC: O(N) SC: O(1)
 var lengthOfLastWord = function(s) {
-    return s.trim().split(" ").at(-1).length;
+    if(s.length === 0) return 0;
+    let foundWord = false;
+    let count = 0;
+    for(let i=s.length - 1; i>=0; i--) {
+        if(s[i] !== ' ') {
+            count++;
+            foundWord = true;
+        }
+        else if(foundWord) {
+            return count;
+        }
+    }
+    return count;
+};
+
+ //TC: O(N) SC: O(N)
+var lengthOfLastWord = function(s) {
+    if(s.length === 0) return 0;
+    return s.trim().split(' ').pop().length;
 };
