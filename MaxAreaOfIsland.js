cbe4c5b5f2ddda8910201827d5c9564b267a502c
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-04-21 12:30:58 +0530

    Update MaxAreaOfIsland.js

--- MaxAreaOfIsland.js
+++ MaxAreaOfIsland.js
@@ -2,34 +2,32 @@
  * @param {number[][]} grid
  * @return {number}
  */
-
-
-const islandArea = (grid, i, j, islandDetails) => {
-    if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === 0) {
-        return;
-    }
-    grid[i][j] = 0;
-    islandDetails.compare = islandDetails.compare + 1;
-    islandDetails.max = Math.max(islandDetails.max, islandDetails.compare);
-     islandArea(grid, i + 1, j, islandDetails);
-     islandArea(grid, i - 1, j, islandDetails);
-     islandArea(grid, i, j + 1, islandDetails);
-     islandArea(grid, i, j - 1, islandDetails);
-}
-
-
 var maxAreaOfIsland = function(grid) {
-    const islandDetails = {
-        max: 0,
-        compare: 0
+
+    const calculateArea = (grid, i, j) => {
+        let totalArea = 0;
+        const islandArea = (grid, i, j) => {
+            if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === 0) {
+                return;
+            }
+            totalArea += 1;
+            grid[i][j] = 0;
+            islandArea(grid, i + 1, j);
+            islandArea(grid, i, j + 1);
+            islandArea(grid, i - 1, j);
+            islandArea(grid, i, j - 1);
+        }
+        islandArea(grid, i, j);
+        return totalArea;
     }
+
+    let maxArea = 0;
     for(let i=0; i<grid.length; i++) {
-        for(let j=0; j<grid[0].length; j++) {
+        for(let j=0; j<grid[i].length; j++) {
             if(grid[i][j] === 1) {
-                islandArea(grid, i, j, islandDetails);
-                islandDetails.compare = 0;
+                maxArea = Math.max(maxArea, calculateArea(grid, i, j));
             }
         }
     }
-    return islandDetails.max;
+    return maxArea;
 };
