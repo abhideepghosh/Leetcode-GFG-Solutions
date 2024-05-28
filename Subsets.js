f64bc17a6923defb9ed9304f926967aede3bdad0
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-05-28 18:22:10 +0530

    Update Subsets.js

--- Subsets.js
+++ Subsets.js
@@ -1,16 +1,36 @@
 /**
- * @param {number[]} nums
+ * TC: O(n*2^n) SC: O(n)
+ * @param {number[]} nums 
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    const result = [[]];
-    for(let num of nums) {
-        const size = result.length;
-        for(let i=0; i<size; i++){
-        const subset = [...result[i]];
-        subset.push(num);
-        result.push(subset);
+    const result = []; // This will hold all the subsets
+    const subset = []; // Temporary array to build each subset
+
+    /**
+     * Backtracking function to generate subsets
+     * @param {number} index - The current starting index to consider for the subset
+     */
+    const backtrack = (index) => {
+        // Add the current subset to the result
+        result.push([...subset]);
+
+        // Iterate over the elements starting from 'index'
+        for(let i = index; i < nums.length; i++) {
+            // Include nums[i] in the current subset
+            subset.push(nums[i]);
+            
+            // Move on to the next element
+            backtrack(i + 1);
+            
+            // Backtrack: remove the last element to try the next option
+            subset.pop();
         }
-    }
+    };
+
+    // Start backtracking from the first index
+    backtrack(0);
+
+    // Return the complete list of subsets
     return result;
 };
