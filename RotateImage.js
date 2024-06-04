0e5fb150cceaa9cb710f8a061632c997c6139f8f
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-06-04 16:38:48 +0530

    Update RotateImage.js

--- RotateImage.js
+++ RotateImage.js
@@ -2,61 +2,27 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-
-const swap = (nums, i, j) => {
-    const temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-}
-
-const reverse = (nums) => {
-    let start = 0;
-    let end = nums.length - 1;
-    while(start < end) {
-        swap(nums, start, end);
-        start++;
-        end--;
-    }
-}
-
+// TC: O(n^2) SC: O(1)
 var rotate = function(matrix) {
+    const reverse = (nums) => {
+        let start = 0, end = nums.length - 1;
+        while(start < end) {
+            const temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
     for(let i=0; i<matrix.length; i++) {
         for(let j=i; j<matrix[0].length; j++) {
             const temp = matrix[i][j];
             matrix[i][j] = matrix[j][i];
             matrix[j][i] = temp;
         }
+        
     }
     for(let i=0; i<matrix.length; i++) {
         reverse(matrix[i]);
     }
-};
-
-
-
-
-// Another Approach
-/**
- * @param {number[][]} matrix
- * @return {void} Do not return anything, modify matrix in-place instead.
- */
-var rotate = function(matrix) {
-    for(let i=0; i<matrix.length; i++) {
-        for(let j=i; j<matrix[i].length; j++) {
-            const temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
-        }
-    }
-
-    const swap = (nums, i, j) => {
-        const temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    for(let i=0; i<matrix.length; i++) {
-        matrix[i].reverse();
-    }
-
 };
