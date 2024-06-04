4f3e2d301289db32170ccfe6b46c25acb983a520
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-06-04 15:51:47 +0530

    Update SetMatrixZeroes.js

--- SetMatrixZeroes.js
+++ SetMatrixZeroes.js
@@ -2,19 +2,29 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-// TC: O(N^2) SC: O(1)
+ // TC: O(N) SC: O(1)
 var setZeroes = function(matrix) {
-    let isColumnOne = true, rows = matrix.length, cols = matrix[0].length;
+    
+    let isColZero = false;
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    
     for(let i=0; i<rows; i++) {
-        if(matrix[i][0] === 0) isColumnOne = false;
+        if(matrix[i][0] === 0) isColZero = true;
         for(let j=1; j<cols; j++) {
-            if(matrix[i][j] === 0) matrix[i][0] = matrix[0][j] = 0;
+            if(matrix[i][j] === 0) {
+                matrix[0][j] = matrix[i][0] = 0;
+            }
         }
     }
+    
     for(let i=rows - 1; i>=0; i--) {
-        for(let j=cols - 1; j>=1; j--) {
-            if(matrix[i][0] === 0 || matrix[0][j] === 0) matrix[i][j] = 0;
+        for(let j=cols - 1; j>0; j--) {
+            if(matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
         }
-        if(!isColumnOne) matrix[i][0] = 0;
+        if(isColZero) matrix[i][0] = 0;
     }
+
 };
