64d6425a6c0b238d8e630d3d399e5f7c42eda9da
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-06-13 11:15:02 +0530

    Update BinaryTreeInorderTraversal.js

--- BinaryTreeInorderTraversal.js
+++ BinaryTreeInorderTraversal.js
@@ -2,7 +2,7 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-
+// TC: O(N) SC: O(N) Recursion
 const traversal = (root) => {
     if(!root) return [];
     
@@ -21,4 +21,23 @@ const traversal = (root) => {
 
 var inorderTraversal = function(root) {
     return traversal(root);
+};
+
+
+
+//TC: O(N) SC: O(N) Iteration
+var inorderTraversal = function(root) {
+    const result = [];
+    const stack = [];
+    let current = root;
+    while(current || stack.length > 0) {
+        while(current) {
+            stack.push(current);
+            current = current.left;
+        }
+            current = stack.pop();
+            result.push(current.val);
+            current = current.right;
+    }
+    return result;
 };
