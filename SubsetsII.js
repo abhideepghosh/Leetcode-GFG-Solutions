af16ebd281b63684097fd67e016aa172a41ad591
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-09-15 16:32:18 +0530

    Update SubsetsII.js

--- SubsetsII.js
+++ SubsetsII.js
@@ -2,23 +2,22 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-// TC: O(n * 2^n) SC: O(n)
 var subsetsWithDup = function(nums) {
+    nums.sort((a,b) => a - b);
 
-    nums.sort((a, b) => a - b);
-    
     const result = [];
-    const subset = [];
-
-    const backtrack = (index) => {
-        result.push([...subset]);
-        for(let i=index; i<nums.length; i++) {
-            if(i > index && nums[i] === nums[i - 1]) continue;
-            subset.push(nums[i]);
-            backtrack(i + 1);
-            subset.pop();
+    
+    const backtrack = (currentIdx, store) => {
+        if(store.length > nums.length) return;
+        result.push([...store]);
+        for(let i=currentIdx; i<nums.length; i++) {
+            if(i === currentIdx || nums[i] !== nums[i - 1]) {
+                store.push(nums[i]);
+                backtrack(i + 1, store);
+                store.pop();
+            }
         }
     }
-    backtrack(0);
+    backtrack(0, []);
     return result;
 };
