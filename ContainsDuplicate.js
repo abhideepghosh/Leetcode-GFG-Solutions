104361043c5f05552477300bc776e0a4b61d7c8f
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-09-19 09:00:05 +0530

    Update ContainsDuplicate.js

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -2,6 +2,23 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// TC: O(N) SC: O(N)
 var containsDuplicate = function(nums) {
-    return !(new Set(nums).size === nums.length);
+    const set  = new Set();
+    for(let num of nums) {
+        if(set.has(num)) return true;
+        else set.add(num);
+    }
+    return false;
+};
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+// TC: O(N) SC: O(N)
+var containsDuplicate = function(nums) {
+    return (new Set(nums)).size !== nums.length;
 };
