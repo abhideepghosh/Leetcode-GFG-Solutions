733f93da8c62b646763185e3709a5ce735defb55
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-09-21 12:43:26 +0530

    Update TopKFrequentElements.js

--- TopKFrequentElements.js
+++ TopKFrequentElements.js
@@ -1,3 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+ // TC: O(N) SC: O(N)
+var topKFrequent = function(nums, k) {
+    const freqMap = new Map();
+    nums.forEach((num, i) => {
+        freqMap.set(num, (freqMap.get(num) || 0) + 1);
+    });
+    const buckets = [];
+    for(let i=0; i<=nums.length; i++) {
+        buckets.push([]);
+    }
+    for(let [num, freq] of freqMap) {
+        buckets[freq].push(num);
+    }
+    const result = [];
+    for(let i=buckets.length - 1; i>=0; i--) {
+        if(buckets[i]) result.push(...buckets[i]);
+    }
+    return result.slice(0, k);
+};
+
+
+
+// TC: O(NlogN) SC: O(N)
 var topKFrequent = function(nums, k) {
     
     const freqMap = new Map();
