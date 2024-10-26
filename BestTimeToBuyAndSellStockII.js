2c1fdcd33b26f4d42ba8b90b2d1c95e5ffdcd58d
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-10-26 12:09:39 +0530

    Update BestTimeToBuyAndSellStockII.js

--- BestTimeToBuyAndSellStockII.js
+++ BestTimeToBuyAndSellStockII.js
@@ -6,7 +6,9 @@
 var maxProfit = function(prices) {
     let profit = 0;
     for(let i=1; i<prices.length; i++) {
-        if(prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        if(prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
     }
     return profit;
 };
