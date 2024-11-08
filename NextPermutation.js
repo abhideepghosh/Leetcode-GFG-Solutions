896546df49a7c3cf56581b2c67e12c4eeedc36bd
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2024-11-08 19:12:15 +0530

    Update NextPermutation.js

--- NextPermutation.js
+++ NextPermutation.js
@@ -2,29 +2,49 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-
-const swap = (nums, i, j) => {
+ // TC: O(N) SC: O(1)
+var nextPermutation = function (nums) {
+  /**
+   * Swaps two elements in the array.
+   * @param {number} i Index of the first element.
+   * @param {number} j Index of the second element.
+   */
+  const swap = (i, j) => {
     const temp = nums[i];
     nums[i] = nums[j];
     nums[j] = temp;
-}
+  };
 
-const reverse = (nums, i) => {
+  /**
+   * Reverses a portion of the array in-place.
+   * @param {number} start Start index of the portion to reverse (inclusive).
+   * @param {number} end End index of the portion to reverse (inclusive).
+   */
+  const reverse = (start, end) => {
+    while (start < end) {
+      swap(start, end);
+      start++;
+      end--;
+    }
+  };
+
+  let i = nums.length - 2;
+
+  // 1. Find the first decreasing element from the right
+  while (i >= 0 && nums[i] >= nums[i + 1]) {
+    i--;
+  }
+
+  if (i >= 0) {
     let j = nums.length - 1;
-    while(i < j) {
-        swap(nums, i, j);
-        i++;
-        j--;
+    // 2. Find the smallest element to the right of nums[i] that is larger than nums[i]
+    while (j >= 0 && nums[i] >= nums[j]) { // >= is important for cases with duplicates
+      j--;
     }
-}
+    // 3. Swap nums[i] and nums[j]
+    swap(i, j);
+  }
 
-var nextPermutation = function(nums) {
-    let lastIndex = nums.length - 2;
-    let swapIndex = nums.length - 1;
-    while(lastIndex >= 0 && nums[lastIndex] >= nums[lastIndex + 1]) lastIndex--;
-    if(lastIndex >= 0) {
-        while(nums[swapIndex] <= nums[lastIndex]) swapIndex--;
-        swap(nums, lastIndex, swapIndex);
-    }
-    reverse(nums, lastIndex + 1);
+  // 4. Reverse the subarray to the right of index i
+  reverse(i + 1, nums.length - 1);
 };
