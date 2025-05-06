d86bd18e0f6606433245c60ff9b51aa5f540d64c
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2025-05-06 18:50:03 +0530

    Update SearchInRotatedSortedArray.js

--- SearchInRotatedSortedArray.js
+++ SearchInRotatedSortedArray.js
@@ -5,27 +5,23 @@
  */
 // TC: O(logN) SC: O(1)
 var search = function(nums, target) {
-   
-   const binarySearch = (start, end) => {
-    while(start <= end) {
-        const mid = start + Math.floor((end - start) / 2);
-        if(nums[mid] === target) return mid;
-        else if(nums[mid] < target) start = mid + 1;
-        else end = mid - 1;
+    const binarySearch  = (nums, start, end, target) => {
+        while(start <= end) {
+            const mid = start + Math.floor((end - start) / 2);
+            if(nums[mid] === target) return mid;
+            else if(nums[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        return -1;
     }
-    return -1;
-   }
-
-   let pivotIdx = 0;
-   let start = 0, end = nums.length - 1;
-   while(start < end) {
-    const mid = start + Math.floor((end - start) / 2);
-    if(nums[mid] < nums[end]) end = mid;
-    else start = mid + 1;
-   }
-   pivotIdx = start;
-   let idx = -1;
-   if(target >= nums[0] && target <= nums[pivotIdx - 1]) idx = binarySearch(0, pivotIdx - 1);
-   else if(target >= nums[pivotIdx] && target <= nums[nums.length - 1]) idx = binarySearch(pivotIdx, nums.length - 1);
-   return idx;
+    const pivot = ((start, end) => {
+        while(start < end){
+            const mid = start + Math.floor((end - start) / 2);
+            if(nums[mid] < nums[end]) end = mid;
+            else start = mid + 1; 
+        }
+        return start;
+    })(0, nums.length - 1);
+    if(target <= nums[nums.length - 1]) return binarySearch(nums, pivot, nums.length - 1, target);
+    else return binarySearch(nums, 0, pivot - 1, target); 
 };
