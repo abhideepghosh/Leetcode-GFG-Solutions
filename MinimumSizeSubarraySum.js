1122e0427f7711fb89f60f4ad96bad8885de629a
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2025-05-21 12:37:26 +0530

    Update MinimumSizeSubarraySum.js

--- MinimumSizeSubarraySum.js
+++ MinimumSizeSubarraySum.js
@@ -1,3 +1,5 @@
+// Both the solutions pass all the test cases. They are 2 ways to code the two pointer technique.
+
 /**
  * @param {number} target
  * @param {number[]} nums
@@ -5,13 +7,42 @@
  */
 // TC: O(N) SC: O(1)
 var minSubArrayLen = function(target, nums) {
-    let minLength = Infinity, pointer = 0, sum = 0;
-    nums.forEach((num, i) => {
-        sum += num;
-        while(sum >= target) {
-            minLength = Math.min(minLength, i + 1 - pointer);
-            sum -= nums[pointer++];
+    let start = 0;
+    let sum = 0;
+    let minLength = Infinity;
+
+    for (let end = 0; end < nums.length; end++) {
+        sum += nums[end];
+
+        while (sum >= target) {
+            minLength = Math.min(minLength, end - start + 1);
+            sum -= nums[start];
+            start++;
         }
-    });
-    return (minLength === Infinity) ? 0 : minLength;
+    }
+
+    return minLength === Infinity ? 0 : minLength;
+};
+
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+// TC: O(N) SC: O(1)
+var minSubArrayLen = function(target, nums) {
+    let start = 0, end = 0;
+    let currentSum = 0, maxLength = Infinity;
+    while(start <= end && end < nums.length) {
+        if(currentSum < target) {
+            currentSum += nums[end];
+        }
+        if(currentSum >= target) {
+            maxLength = Math.min(maxLength, end - start + 1);
+            currentSum -= nums[start];
+            start++;
+        }
+        if(currentSum < target) end++;
+    }
+    return maxLength === Infinity ? 0 : maxLength;
 };
