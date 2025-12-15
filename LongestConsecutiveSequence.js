cfe81bf8dc04f032968203d5e83c34babaf5fc9e
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2025-12-15 12:58:08 +0530

    Optimize longestConsecutive function implementation
    Refactor longestConsecutive function to improve performance and clarity.

--- LongestConsecutiveSequence.js
+++ LongestConsecutiveSequence.js
@@ -2,7 +2,30 @@
  * @param {number[]} nums
  * @return {number}
  */
-//TC: O(N) SC: O(N)
+// TC: O(N) SC: O(N)
+var longestConsecutive = function(nums) {
+    const set = new Set(nums);
+    let maxSeq = 0;
+    set.forEach((num) => {
+        if(!set.has(num - 1)) {
+            let currSeq = 1;
+            while(set.has(num + 1)) {
+                currSeq++;
+                num++;
+            }
+            maxSeq = Math.max(maxSeq, currSeq);
+        }
+    });
+    return maxSeq;
+};
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+//TC: O(N^2) SC: O(N)
 var longestConsecutive = function(nums) {
     const set = new Set(nums);
     let maxSize = 0;
